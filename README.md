d0dd4caea8615a2195e6082eb1a8a53ec5da9c63
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-18 21:14:57 +0300

    Added Read Me

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # phase-1-wk3-Code-Challenge
 phase 1 WK3 Code Challenge
+
+## Introduction
+This small app adds items to a list, clears them and indicates if they are purchased or not.
+
+## Functions
+1. Create list items
+    The first function adds items in the list and also ads an [Id] [#item] to be able to target the items easier with the clear button to be added later.
+
+2. Add list items
+    The second function is to add items to the list from the [inputfield]. All items the user keys in will appear in the form of a [ul] list. They appear with an [id] to be able to target them specifically.
+
+3. Purchase list items
+    The third function is to mark items as being purchased. I achieve this by addding a [strike] through element onto the text to be able to mark them as purchsed.
+
+4. Clear list items
+    The last function is to clear our list as there needs to be a way to refresh our list without reloading the page. This can be done by using our [#item] id to clear our list and allow the user to start over.
+
+# Conclusion
+This app is a simple one and does not have alot of styling, but we can make it more complex using css or some bootstrap. I will continue to update as we go.
--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-    
+// function to create elements in the list 
 function content(list){
     const li = document.createElement('li')
     li.textContent = list
     li.id = '#item'
     document.querySelector('#shopping-list').appendChild(li)
+
+    // create the clear button functionality
+
+    const clearBtn = document.querySelector('#clear-btn')
+    clearBtn.addEventListener('click', function(e){
+        e.preventDefault()
+        const clearList = document.querySelector('#items')
+        clearList.target.parentNode.remove()
+    })
+
+    // Create the functionality to indicate items purchased by striking them through
+
+    const purchaseBtn = document.querySelector('#purchase-btn')
+    purchaseBtn.addEventListener('click', function(e){
+        const strke = document.createElement<'strike'>
+        document.querySelectorAll('#item').appendChild()
+    })
 }
 
+// Creates the add items button functionality to add the value in the input field to the list
 
 const add = document.querySelector('#add-btn')
 add.addEventListener('click', function(e){
     e.preventDefault()
     const listItems = document.querySelector('#text-box')
     content(listItems.value)
-})
-
-const dltBtn = document.querySelector('#clear-list-btn').addEventListener('click', function(){
-    const clear = document.querySelectorAll('#item')
-    clear.forEach((e)=>{
-        e.parentElement.remove()
-    })
-})
-
-const purchased = document.querySelector('#purchase-btn')
-purchased.addEventListener('click', function(e){
-    const strke = value.strike()
-    document.getElementById('#shopping-list').appendChild(strke) 
 })
