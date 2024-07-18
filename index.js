12c215c7377858fccdb67f705aeedb4da1932a90
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-18 23:17:47 +0300

    Changed Code

--- index.js
+++ index.js
@@ -1,60 +1,37 @@
-let items = [];
-const itemDisplay = document.getElementById('list')
-const itemInput = document.getElementById('shopping-list')
+// function to create elements in the list 
+function content(list){
+    const li = document.createElement('li')
+    li.textContent = list
+    li.id = '#item'
+    document.querySelector('#shopping-list').appendChild(li)
 
-//Get references to buttons and add event listeners
-const addItemButton = document.getElementById('addBtn')
-addItemButton.addEventListener('click', () => {
-    addItem()
-});
+    // Create the functionality to indicate items purchased by striking them through
+const purchaseBtn = document.querySelector('#purchase-btn')
+purchaseBtn.addEventListener('click', function(e){
+    const strke = document.createElement<'strike'>
+    document.querySelectorAll('#item').appendChild()
+})
 
+// create the clear button functionality
+const addClearBtn = document.getElementById('addclear-btn')
+addClearBtn.addEventListener('click', ()=>{
 
-const addClearList = document.getElementById('clearBtn')
-addClearList.addEventListener('click', () => {
-    clearList()
-});
-
-const addMarkAsPurchased = document.getElementById('purchaseBtn')
-addMarkAsPurchased.addEventListener('click', () => {
-    markAsPurchased()
-});
-
-//function to add item to the list
-function addItem() {
-    items.push({
-        name: itemInput.value,
-        purchased: false
-    })
-    console.log(items.length)
-    displayList()
-}
-
-//this function is used to refresh items on the list view.
-function displayList() {
-
-    console.log(items)
-    itemDisplay.innerHTML = '' // clear current list
-
-    //Loop through items array and create list items
-    for (let i = 0; i < items.length; i++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = items[i].name
-        itemDisplay.appendChild(listItem)
-        listItem.classList.toggle('purchased', items[i].purchased)
-    }
-
+})
 
 }
-//add a function ClearList (indorder to clear the list)
-function clearList() {
-    items = []
-    displayList()
 
+function addItems(){
+    list = []
+    content()
 }
-//add function markAsPurchased
-function markAsPurchased() {
-    for (let i = 0; i < items.length; i++) {
-        items[i].purchased = true; // sets status of all purchased items as true
-    }
-    displayList() //// Update the displayed list to reflect changes
-}
+
+// Creates the add items button functionality to add the value in the input field to the list
+const add = document.querySelector('#add-btn')
+add.addEventListener('click', function(e){
+    e.preventDefault()
+    const listItems = document.querySelector('#text-box')
+    content(listItems.value)
+})
+
+
+
